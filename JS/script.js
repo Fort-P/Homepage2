bc4fc6de220117968f6eef14e85872c93d01f6a0
Fort-P <74835871+Fort-P@users.noreply.github.com>
2023-05-18 22:12:56 -0400

    Removed jQuery, Added Logo Variations, Added script.js
    Removed jQuery
    Added script,js to handle all general purpose JS
    Finished main page (index.js)
    General formatting to multiple files

---
+++ JS/script.js
@@ -0,0 +1,10 @@
+const faqQuestions = document.querySelectorAll(".faq");
+const faqAnswers = document.querySelectorAll(".faq-a");
+const faqIcons = document.querySelectorAll(".faq-icon");
+
+faqQuestions.forEach((faqQuestion, index) => {
+  faqQuestion.addEventListener("click", () => {
+    faqIcons[index].classList.toggle("expand");
+    faqAnswers[index].classList.toggle("expand");
+  });
+});
