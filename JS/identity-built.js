5f10a63f8d767ae57429e135b77003910d066314
Fort-P <74835871+Fort-P@users.noreply.github.com>
2023-05-21 14:57:23 -0400

    added built js

---
+++ JS/identity-built.js
@@ -0,0 +1,12 @@
+(() => {
+  // node_modules/gotrue-js/index.d.ts
+  var index_d_default = GoTrue;
+
+  // JS/identity.js
+  auth = new index_d_default({
+    APIUrl: "https://homepage2.netlify.app/.netlify/identity",
+    audience: "",
+    setCookie: false
+  });
+})();
+//# sourceMappingURL=identity-built.js.map
--- JS/identity.js
+++ JS/identity.js
@@ -3,7 +3,7 @@ import GoTrue from './../node_modules/gotrue-js/index.d.ts';
 // Instantiate the GoTrue auth client with an optional configuration
 
 auth = new GoTrue({
-  APIUrl: 'https://<your domain name>/.netlify/identity',
+  APIUrl: 'https://homepage2.netlify.app/.netlify/identity',
   audience: '',
   setCookie: false,
 });
